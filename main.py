cda9696ae7178e012fa417b537e9c4ec9a3824e6
Nik-Cryptic <98311369+ErrorNik14@users.noreply.github.com>
2022-03-10 14:34:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import nextcord
 from nextcord.ext import commands
+import os
 
 intents = nextcord.Intents()
 intents.members = True
 bot = commands.Bot(command_prefix="d!", intents=intents, case_insensitive=True)
-bot.remove_command(help)
+bot.remove_command('help')
 
 @bot.event
 async def on_connect():
@@ -12,3 +13,5 @@ async def on_connect():
 
 
 bot.run("TOKEN")
+#Use the bot token which is in the group dm ping when testing or running bot,
+#but ALWAYS change it back to bot.run(os.)
